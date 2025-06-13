57fd5ee477147b575292e6de3b09bccdb02f8c50
Szilárd <mate.szilard.mark@gmail.com>
2025-06-13 10:25:57 +0200

    f

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -7,9 +7,9 @@ import Link from "next/link";
 export default function Hero() {
   return (
     <section className="from-muted to-background relative bg-gradient-to-tr px-4 md:px-10">
-      <div className="m-auto grid max-w-6xl gap-8 border-x px-2 pt-10 pb-20 md:px-10 lg:grid-cols-2 lg:gap-8">
+      <div className="m-auto grid max-w-6xl gap-8 border-x px-2 pt-10 pb-20 md:px-10 lg:grid-cols-1 lg:gap-8">
         <div className="from-background to-background/0 absolute top-0 left-0 z-10 h-10 w-full bg-gradient-to-b" />
-        <div className="flex flex-col items-center lg:items-start">
+        <div className="flex flex-col items-center lg:items-center">
           <Badge variant="outline" className="mb-2">
             <Code size={12} className="mr-1" />
             Product-Building Team for Creators & Experts
@@ -20,7 +20,7 @@ export default function Hero() {
               Scalable Products
             </span>
           </h1>
-          <p className="text-muted-foreground mb-3 text-center md:text-lg lg:text-left">
+          <p className="text-muted-foreground mb-3 text-center text-balance md:text-lg">
             We help creators, coaches, and niche entrepreneurs turn their ideas, expertise, and audience into real,
             revenue-generating platforms — built fast, and built to last.
           </p>
@@ -47,25 +47,16 @@ export default function Hero() {
             </Button>
           </div>
         </div>
-        <div className="relative my-auto flex aspect-[1.7777777777777777/1] h-fit items-center overflow-hidden rounded-lg drop-shadow-[0_0_21px_rgba(255,255,255,0.2),0_0_10px_rgba(255,93,1,0.2)]">
+        {/*         <div className="relative my-auto flex aspect-[1.7777777777777777/1] h-fit items-center overflow-hidden rounded-lg drop-shadow-[0_0_21px_rgba(255,255,255,0.2),0_0_10px_rgba(255,93,1,0.2)]">
           <Script src="https://fast.wistia.com/player.js" async />
           <Script src="https://fast.wistia.com/embed/q91qufujce.js" async type="module" />
-          <style>
-            {`
-            wistia-player[media-id='q91qufujce']:not(:defined) {
-                background: center / contain no-repeat url('https://fast.wistia.com/embed/medias/q91qufujce/swatch');
-                display: block;
-                filter: blur(5px);
-                padding-top:56.25%;
-            }
-            `}
-          </style>
+
           <wistia-player
             className="absolute top-0 left-0"
             media-id="q91qufujce"
             aspect="1.7777777777777777"
           ></wistia-player>
-        </div>
+        </div> */}
       </div>
 
       <hr className="absolute bottom-14 left-0 w-full" />
